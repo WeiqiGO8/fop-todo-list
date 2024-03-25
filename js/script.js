03c07780fe3459e34a3f99c41ef4cbbf841d09ed
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-03-25 18:57:13 +0100

    small changes
    minor changes in html and css, started a little on javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let submitTextElement = document.getElementById("submit-tex");
+let submitBtnElement = document.getElementsById("submit-btn");
+
+let listUnorderedElement = [];
+
+function addTodo() {
+  submitBtnElement.document.createElement("li");
+}
+
+function finishedTodo() {}
+
+function removeTodo() {}
