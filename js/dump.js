e0f0cb78c3618c68bd85cc58c300ac7266a66a77
Hannah Jansson <hannah.lingzhi@gmail.com>
2024-04-10 16:48:25 +0200

    now localStorage stores everything in one array

--- js/dump.js
+++ js/dump.js
@@ -1,60 +1,135 @@
-// retreve the html elements to js
+// get html elements
 const numberElement = document.getElementById("number");
 const inputTextElement = document.getElementById("input-text");
 const inputBtnElement = document.getElementById("input-btn");
 const todoUlElement = document.getElementById("todo-ul");
 
-// array for the li element and checked li elements
 // the following 2 lines of code was adapted from ChatGPT: https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
 let todoListArray = JSON.parse(localStorage.todoTask || "[]");
 let checkedListArray = JSON.parse(localStorage.checkedTask || "[]");
 
-// add input text to the list and localStorage
+let numberOfTodo = 0;
+
+// the following 15 lines of code was adapted from:
+// https://www.shecodes.io/athena/42851-how-to-create-a-to-do-list-using-html-css-and-javascript - 2024-03-28
+// https://chat.openai.com/share/f2900035-a174-4d04-b8d4-c89a610de4a6 - 2024-03-28
+// https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
+
 function addTodoTask() {
   const inputValue = inputTextElement.value;
 
-  createElements(inputValue);
-  todoListArray.push(inputValue);
-  localStorage.setItem("todoTask", JSON.stringify(todoListArray));
-  inputTextElement.value = "";
+  // if input field is filled - create elements for the value
+  if (inputValue !== "") {
+    todoListArray.push(inputValue);
+    localStorage.todoTask = JSON.stringify(todoListArray);
+
+    // Update the page
+    createElements(inputValue);
+    inputTextElement.value = "";
+  } else {
+    createElements.preventDefault();
+  }
 }
 
-// save checked todo task to localStorage in checkedListArray
-function saveCheckedTodoTask() {}
-
-// remove todo task from the list and localStorage
-function removeTodoTask() {}
-
-// the following 19 lines of code was adapted from a combination of:
+// The following 24 lines of code was adapted from
 // https://www.shecodes.io/athena/42851-how-to-create-a-to-do-list-using-html-css-and-javascript - 2024-03-28
-// https://chat.openai.com/share/0eaaa72e-c4ba-4352-876b-464ab3956809 - 2024-04-05
+// https://chat.openai.com/share/f2900035-a174-4d04-b8d4-c89a610de4a6 - 2024-03-28
 function createElements(inputValue) {
+  // create elements for the input value
+  const inputCheckboxElement = document.createElement("input");
+  inputCheckboxElement.type = "checkbox";
+  inputCheckboxElement.classList.add("checkedbox");
+  inputCheckboxElement.addEventListener("change", checkTodo);
+
   const liElement = document.createElement("li");
-  const textNode = document.createTextNode(inputValue);
 
-  const inputCheckboxBtnElement = document.createElement("input");
-  inputCheckboxBtnElement.type = "checkbox";
-  inputCheckboxBtnElement.classList.add("checkedbox");
-  inputCheckboxBtnElement.addEventListener("change", styleELements);
-
+  //delete button
   const deleteInputBtnElement = document.createElement("input");
   deleteInputBtnElement.type = "button";
-  deleteInputBtnElement.classList.add("deletebox");
   deleteInputBtnElement.value = "❌";
+  deleteInputBtnElement.classList.add("deletebox");
+  deleteInputBtnElement.addEventListener("click", deleteTodoTask);
+
+  const textNode = document.createTextNode(inputValue);
 
   todoUlElement.appendChild(liElement);
-  liElement.appendChild(textNode);
-  liElement.appendChild(inputCheckboxBtnElement);
+  liElement.appendChild(inputCheckboxElement);
   liElement.appendChild(deleteInputBtnElement);
+  liElement.appendChild(textNode);
+  updateTodoNumber();
 }
 
-function styleELements(event) {
-  const inputCheckboxBtnElement = event.target;
-  const liElement = inputCheckboxBtnElement.parentNode;
+// toggle finished/not finished todo-list items class style ✅
+// The following 19 lines of code was adapted from:
+// https://chat.openai.com/share/0f3b3bb7-93c0-4adf-a355-e52a35bb5473 - 2024-03-28
+// https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
+function checkTodo(event) {
+  let inputCheckboxElement = event.target;
+  let liElement = inputCheckboxElement.parentNode;
+
+  if (inputCheckboxElement.checked === true) {
+    liElement.classList.add("check");
+    checkedListArray.push(liElement.innerText);
+    localStorage.checkedTask = JSON.stringify(checkedListArray);
+  } else {
+    liElement.classList.remove("check");
+
+    // the following 5 lines of code was adapted from: https://chat.openai.com/share/454e06e4-c555-4ea3-93b0-da0556993904 - 2024-04-05
+    let index = checkedListArray.indexOf(liElement.innerText);
+    if (index !== -1) {
+      checkedListArray.splice(index, 1);
+      localStorage.checkedTask = JSON.stringify(checkedListArray);
+    }
+  }
 }
 
-function loadHandler() {
+// the following 13 lines of code was adapted from: https://chat.openai.com/share/454e06e4-c555-4ea3-93b0-da0556993904 - 2024-04-05
+function saveCheckTodo() {
+  // Mark todo items as checked and apply styling
+  checkedListArray.forEach(function (todoText) {
+    let todoItems = todoUlElement.getElementsByTagName("li");
+    for (let i = 0; i < todoItems.length; i++) {
+      if (todoItems[i].textContent.trim() === todoText.trim()) {
+        let checkbox = todoItems[i].querySelector("input[type=checkbox]");
+        checkbox.checked = true;
+        todoItems[i].classList.add("check");
+      }
+    }
+  });
+}
+
+// remove the checked-style class and remove the list item from the list ❌
+// the following 17 lines of code was adapted from: https://chat.openai.com/share/4ca0cbad-71ae-4c4d-bfb6-f32ea30ee7ac - 2024-04-05
+function deleteTodoTask() {
+  const liElement = this.parentNode;
+  const todoText = liElement.innerText;
+
+  let index = todoListArray.indexOf(todoText);
+  if (index !== -1) {
+    todoListArray.splice(index, 1);
+    localStorage.todoTask = JSON.stringify(todoListArray);
+  }
+
+  let checkedIndex = checkedListArray.indexOf(todoText);
+  if (checkedIndex !== -1) {
+    checkedListArray.splice(checkedIndex, 1);
+    localStorage.checkedTask = JSON.stringify(checkedListArray);
+  }
+  liElement.parentNode.removeChild(liElement);
+  updateTodoNumber();
+}
+
+function updateTodoNumber() {
+  numberElement.innerText = "Number: " + todoListArray.length;
+}
+
+function loadPageHandler() {
   inputBtnElement.addEventListener("click", addTodoTask);
+  // the following 2 lines of code was adapted from: https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
+  todoListArray.forEach(createElements);
+  checkedListArray.forEach(saveCheckTodo);
+  updateTodoNumber();
 }
 
-loadHandler();
+window.addEventListener("load", loadPageHandler);
+window.addEventListener("storage", loadPageHandler);
--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ const todoUlElement = document.getElementById("todo-ul");
 
 // the following 2 lines of code was adapted from ChatGPT: https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
 let todoListArray = JSON.parse(localStorage.todoTask || "[]");
-let checkedListArray = JSON.parse(localStorage.checkedTask || "[]");
-
-let numberOfTodo = 0;
 
 // the following 15 lines of code was adapted from:
 // https://www.shecodes.io/athena/42851-how-to-create-a-to-do-list-using-html-css-and-javascript - 2024-03-28
@@ -20,42 +17,48 @@ function addTodoTask() {
 
   // if input field is filled - create elements for the value
   if (inputValue !== "") {
-    todoListArray.push(inputValue);
+    let task = {
+      finished: false,
+      text: inputValue,
+    };
+
+    todoListArray.push(task);
     localStorage.todoTask = JSON.stringify(todoListArray);
 
     // Update the page
-    createElements(inputValue);
+    createElements(task);
     inputTextElement.value = "";
-  } else {
-    createElements.preventDefault();
   }
 }
 
 // The following 24 lines of code was adapted from
 // https://www.shecodes.io/athena/42851-how-to-create-a-to-do-list-using-html-css-and-javascript - 2024-03-28
 // https://chat.openai.com/share/f2900035-a174-4d04-b8d4-c89a610de4a6 - 2024-03-28
-function createElements(inputValue) {
-  // create elements for the input value
+function createElements(task) {
+  const liElement = document.createElement("li");
+
   const inputCheckboxElement = document.createElement("input");
   inputCheckboxElement.type = "checkbox";
   inputCheckboxElement.classList.add("checkedbox");
-  inputCheckboxElement.addEventListener("change", checkTodo);
+  inputCheckboxElement.addEventListener("change", function (event) {
+    checkTodo(task, event);
+  });
 
-  const liElement = document.createElement("li");
-
-  //delete button
   const deleteInputBtnElement = document.createElement("input");
   deleteInputBtnElement.type = "button";
   deleteInputBtnElement.value = "❌";
   deleteInputBtnElement.classList.add("deletebox");
-  deleteInputBtnElement.addEventListener("click", deleteTodoTask);
+  deleteInputBtnElement.addEventListener("click", function () {
+    deleteTodoTask(task);
+  });
 
-  const textNode = document.createTextNode(inputValue);
+  const textNode = document.createTextNode(task.text);
 
   todoUlElement.appendChild(liElement);
   liElement.appendChild(inputCheckboxElement);
   liElement.appendChild(deleteInputBtnElement);
   liElement.appendChild(textNode);
+
   updateTodoNumber();
 }
 
@@ -63,60 +66,46 @@ function createElements(inputValue) {
 // The following 19 lines of code was adapted from:
 // https://chat.openai.com/share/0f3b3bb7-93c0-4adf-a355-e52a35bb5473 - 2024-03-28
 // https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
-function checkTodo(event) {
-  let inputCheckboxElement = event.target;
-  let liElement = inputCheckboxElement.parentNode;
+function checkTodo(task, event) {
+  task.finished = event.target.checked;
+  localStorage.todoTask = JSON.stringify(todoListArray);
 
-  if (inputCheckboxElement.checked === true) {
-    liElement.classList.add("check");
-    checkedListArray.push(liElement.innerText);
-    localStorage.checkedTask = JSON.stringify(checkedListArray);
+  if (event.target.checked === true) {
+    event.target.parentNode.classList.add("check");
   } else {
-    liElement.classList.remove("check");
-
-    // the following 5 lines of code was adapted from: https://chat.openai.com/share/454e06e4-c555-4ea3-93b0-da0556993904 - 2024-04-05
-    let index = checkedListArray.indexOf(liElement.innerText);
-    if (index !== -1) {
-      checkedListArray.splice(index, 1);
-      localStorage.checkedTask = JSON.stringify(checkedListArray);
-    }
+    event.target.parentNode.classList.remove("check");
   }
 }
 
-// the following 13 lines of code was adapted from: https://chat.openai.com/share/454e06e4-c555-4ea3-93b0-da0556993904 - 2024-04-05
-function saveCheckTodo() {
-  // Mark todo items as checked and apply styling
-  checkedListArray.forEach(function (todoText) {
-    let todoItems = todoUlElement.getElementsByTagName("li");
-    for (let i = 0; i < todoItems.length; i++) {
-      if (todoItems[i].textContent.trim() === todoText.trim()) {
-        let checkbox = todoItems[i].querySelector("input[type=checkbox]");
-        checkbox.checked = true;
-        todoItems[i].classList.add("check");
-      }
-    }
-  });
-}
+// let inputCheckboxElement = event.target;
+// let liElement = inputCheckboxElement.parentNode;
+
+// if (liIndex !== -1) {
+//   todoListArray[todoTaskIndex].finished = inputCheckboxElement.checked;
+//   localStorage.todoTask = JSON.stringify(todoListArray);
+// }
 
 // remove the checked-style class and remove the list item from the list ❌
 // the following 17 lines of code was adapted from: https://chat.openai.com/share/4ca0cbad-71ae-4c4d-bfb6-f32ea30ee7ac - 2024-04-05
-function deleteTodoTask() {
-  const liElement = this.parentNode;
-  const todoText = liElement.innerText;
-
-  let index = todoListArray.indexOf(todoText);
-  if (index !== -1) {
-    todoListArray.splice(index, 1);
+function deleteTodoTask(task) {
+  const taskIndex = todoListArray.indexOf(task);
+  if (taskIndex !== -1) {
+    todoListArray.splice(taskIndex, 1);
     localStorage.todoTask = JSON.stringify(todoListArray);
+    event.target.parentNode.remove();
+    updateTodoNumber();
   }
 
-  let checkedIndex = checkedListArray.indexOf(todoText);
-  if (checkedIndex !== -1) {
-    checkedListArray.splice(checkedIndex, 1);
-    localStorage.checkedTask = JSON.stringify(checkedListArray);
-  }
-  liElement.parentNode.removeChild(liElement);
-  updateTodoNumber();
+  // const liElement = this.parentNode;
+  // const todoText = liElement.innerText;
+
+  // if (taskIndex !== -1) {
+  //   todoListArray.splice(taskIndex, 1);
+  //   localStorage.todoTask = JSON.stringify(todoListArray);
+  // }
+
+  // const checkedIndex = todoListArray.indexOf(todoText);
+  // liElement.parentNode.removeChild(liElement);
 }
 
 function updateTodoNumber() {
@@ -127,9 +116,8 @@ function loadPageHandler() {
   inputBtnElement.addEventListener("click", addTodoTask);
   // the following 2 lines of code was adapted from: https://chat.openai.com/share/80637849-86aa-4553-b3ce-0f7f015e6a99 - 2024-04-05
   todoListArray.forEach(createElements);
-  checkedListArray.forEach(saveCheckTodo);
   updateTodoNumber();
 }
 
 window.addEventListener("load", loadPageHandler);
-window.addEventListener("storage", loadPageHandler);
+// window.addEventListener("storage", loadPageHandler);
